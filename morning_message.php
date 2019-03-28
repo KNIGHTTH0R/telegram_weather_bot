848f998c5017562dd9ae1405b14261dfd8e3364b
santoniothefirst <santonio123@mail.ru>
2019-03-28 21:37:00 +0300

    Переименовал

---
+++ morning_message.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once 'Weather.php';
+require_once 'TelegramBot.php';
+
+$bot = new TelegramBot();
+
+$weather = new Weather();
+// $i = true;
+while(true){
+  if(date('H:i') == '21:40' || date('H:i') == '22:40'){
+
+    echo 'Morning message ';
+
+    $freshData = $weather->getWeather('Moscow');
+
+    $description = [];
+
+    foreach ($freshData->weather as $weather_data_array) {
+
+      $description[] = $weather_data_array->description;
+
+    }
+
+    $msg = "Сейчас в городе $freshData->name:";
+
+    foreach ($description as $key => $value) {
+
+      if($key==0) $msg .= ' ' . $value;
+
+      else $msg .= ', ' . $value;
+    }
+
+    $msg .= ".\nТемпература составляет " . round($freshData->main->temp - 273, 0, PHP_ROUND_HALF_UP) . " ℃.\nНе простуди жопу!";
+
+    $bot->sendMessage('-250143773', $msg);
+
+
+    sleep(60);
+  }
+}
+ ?>
